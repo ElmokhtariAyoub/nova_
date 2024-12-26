7f45130c7c1e47776491b5dc0fc02758055b9978
ElmokhtariAyoub <ayoubelmokhtari24@gmail.com>
2024-12-26 16:27:11 +0100

    Add files via upload

---
+++ model/modifClient.php
@@ -0,0 +1,35 @@
+<?php
+include 'connexion.php';
+if (
+    !empty($_POST['nom'])
+    && !empty($_POST['prenom'])
+    && !empty($_POST['telephone'])
+    && !empty($_POST['adresse'])
+    && !empty($_POST['id'])
+) {
+
+$sql = "UPDATE client SET nom=?, prenom=?, telephone=?, adresse=? WHERE id=?";
+    $req = $connexion->prepare($sql);
+    
+    $req->execute(array(
+        $_POST['nom'],
+        $_POST['prenom'],
+        $_POST['telephone'],
+        $_POST['adresse'],
+        $_POST['id']
+    ));
+    
+    if ( $req->rowCount()!=0) {
+        $_SESSION['text'] = "client modifié avec succès";
+        $_SESSION['type'] = "success";
+    }else {
+        $_SESSION['text'] = "Rien a été modifié";
+        $_SESSION['type'] = "warning";
+    }
+
+} else {
+    $_SESSION['text'] ="Une information obligatoire non rensignée";
+    $_SESSION['type'] = "danger";
+}
+
+header('Location: ../vue/client.php');
---
+++ vue/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+include 'entete.php';
+if (empty($_SESSION['admin_authenticated']) || $_SESSION['admin_authenticated'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<?php
+include 'pied.php';
+?>
